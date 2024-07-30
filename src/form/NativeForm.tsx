846ed63801810d48198f81b473c7667664aebac0
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-07-30 16:12:27 +0600

    scema validation implemented

--- src/form/NativeForm.tsx
+++ src/form/NativeForm.tsx
@@ -1,24 +1,38 @@
+import { ErrorMessage } from "@hookform/error-message";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
 type TNativeFormInputs = {
   name: string;
+  email: string;
   age: number;
   dob: Date;
 };
 
+const nativeFormInputSchema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  age: z.number({ required_error: "age must be number" }),
+  dob: z.date({ required_error: "date of birth is required" }),
+});
+
 const NativeForm = () => {
-  const { register, handleSubmit } = useForm<TNativeFormInputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TNativeFormInputs>({
+    resolver: zodResolver(nativeFormInputSchema),
+  });
   const handleOnSubmit: SubmitHandler<TNativeFormInputs> = (data) => {
     alert(JSON.stringify(data));
+    console.log("hello");
   };
 
   return (
     <div className="max-w-xl my-12 mx-auto ">
-      <form
-        action=""
-        className="flex flex-col"
-        onSubmit={handleSubmit(handleOnSubmit)}
-      >
+      <form className="flex flex-col" onSubmit={handleSubmit(handleOnSubmit)}>
         <div className="flex flex-col">
           <label htmlFor="name">Name</label>
           <input
@@ -26,6 +40,21 @@ const NativeForm = () => {
             className="border p-2 my-1"
             placeholder="Name"
             {...register("name")}
+          />
+          {errors && <p className="text-red-400">{errors.name?.message}</p>}
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="border p-2 my-1"
+            placeholder="Email"
+            {...register("email")}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ message }) => <p className="text-red-500">{message}</p>}
           />
         </div>
         <div className="flex flex-col">
@@ -36,6 +65,11 @@ const NativeForm = () => {
             placeholder="Age"
             {...register("age", { valueAsNumber: true })}
           />
+          <ErrorMessage
+            errors={errors}
+            name="age"
+            render={({ message }) => <p className="text-red-500">{message}</p>}
+          />
         </div>
         <div className="flex flex-col">
           <label htmlFor="dob">Date of Birth</label>
@@ -43,8 +77,12 @@ const NativeForm = () => {
             type="date"
             className="border p-2 my-1"
             placeholder="Date of birth"
-            {...register("dob", { valueAsDate: true })}
+            {...register("dob", {
+              valueAsDate: true,
+              required: "this field is required",
+            })}
           />
+          {errors && <p className="text-red-500">{errors.dob?.message}</p>}
         </div>
         <button type="submit" className="bg-blue-600 text-white w-20 p-2 my-2">
           Submit
